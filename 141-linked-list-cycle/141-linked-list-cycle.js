9cda96c9fae14457f64551f89fbdc3513793ce28
Johnny Wu <skydodle@gmail.com>
2022-08-01 14:49:56 -0700

    Time: 122 ms (31.84%), Space: 45 MB (61.54%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+  let turtle = head;
+  let rabbit = head;
+  
+  while (rabbit && rabbit.next) {
+    turtle = turtle.next;
+    rabbit = rabbit.next.next;
+    
+    if (turtle === rabbit) {
+      return true;
+    }
+  }
+  return false;
+};
