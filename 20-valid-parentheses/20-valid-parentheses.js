91c1c02aa39aeb1065e12e81812c0d0e732595da
Johnny Wu <skydodle@gmail.com>
2022-08-22 15:51:31 -0700

    Time: 73 ms (85.58%), Space: 42.3 MB (73.01%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,27 +3,24 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-  // Stack to store left symbols
-  const leftSymbols = [];  
-  // Loop for each character of the string
-  for (var i = 0; i < s.length; i++) {
-    // if left symbol is encountered
-    if (s[i] === '('  || s[i] === '{' || s[i] === '[' ) {
-       leftSymbols.push(s[i]); 
+  if (s.length < 2) {
+    return false
+  }  
+  const leftSymbols = [];
+  for (var i = 0 ; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+      leftSymbols.push(s[i]);
     }
-    // if the right symbol is encountered
-    else if (s[i] === ')' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '(') {
-        leftSymbols.pop();
+    else if (s[i] === ')' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '(') {
+      leftSymbols.pop();
     }
-    else if (s[i] === ']' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '[') {
-        leftSymbols.pop();
+    else if (s[i] === '}' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '{') {
+      leftSymbols.pop();
     }
-    else if (s[i] === '}' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '{') {
-    leftSymbols.pop();
-    }
-    // if none of the symbols is encountered
-    else {
-        return false;
+    else if (s[i] === ']' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '[') {
+      leftSymbols.pop();
+    } else {
+      return false      
     }
   }
   return leftSymbols.length === 0;
