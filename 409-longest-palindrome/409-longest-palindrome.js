b610a5f5a32d0b5a9de2b6d1672816e5dc3540a7
Johnny Wu <skydodle@gmail.com>
2022-08-22 16:29:01 -0700

    Time: 78 ms (84.69%), Space: 43.1 MB (83.86%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function (s) {
+  const counts = {};
+  let sum = 0;
+  let oddPresent = false;
+
+  for (let i = 0; i < s.length; i++) {
+    counts[s[i]] = (counts[s[i]] || 0) + 1;
+  }
+
+  for (let letter in counts) {
+    if (counts[letter] % 2 === 0) {
+      sum += counts[letter];
+    } else {
+      oddPresent = true;
+      sum += counts[letter] - 1;
+    }
+  }
+
+  if (oddPresent) sum += 1;
+
+  return sum;
+};
