54fec1bcec286e018fadcad7a9bf00bdec3e37c2
Johnny Wu <skydodle@gmail.com>
2022-09-02 10:19:50 -0700

    Time: 103 ms (70.01%), Space: 43.3 MB (25.27%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,12 +3,26 @@
  * @param {number} target
  * @return {number[]}
  */
+// var twoSum = function(nums, target) {
+//     for (var i = 0; i < nums.length; i++) {
+//       for (var j = i + 1; j < nums.length; j++) {
+//         if (nums[i] + nums[j] === target) {
+//           return [i, j]
+//         }
+//       }
+//     }
+// };
+
 var twoSum = function(nums, target) {
-    for (var i = 0; i < nums.length; i++) {
-      for (var j = i + 1; j < nums.length; j++) {
-        if (nums[i] + nums[j] === target) {
-          return [i, j]
-        }
-      }
+  const hashTable = {};
+  
+  for (var i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    
+    if (hashTable.hasOwnProperty(complement)) {
+      return [i, hashTable[complement]];
     }
-};
+    hashTable[nums[i]] = i;
+  }
+
+}
