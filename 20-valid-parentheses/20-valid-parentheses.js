bf0ce6edf73ef5d454e29f6fa0cb7d4fa59f14f7
Johnny Wu <skydodle@gmail.com>
2022-09-02 11:12:58 -0700

    Time: 80 ms (76.77%), Space: 42.4 MB (61.98%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -2,26 +2,55 @@
  * @param {string} s
  * @return {boolean}
  */
+// var isValid = function(s) {
+//   if (s.length < 2) {
+//     return false
+//   }  
+//   const leftSymbols = [];
+//   for (var i = 0 ; i < s.length; i++) {
+//     if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+//       leftSymbols.push(s[i]);
+//     }
+//     else if (s[i] === ')' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '(') {
+//       leftSymbols.pop();
+//     }
+//     else if (s[i] === '}' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '{') {
+//       leftSymbols.pop();
+//     }
+//     else if (s[i] === ']' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '[') {
+//       leftSymbols.pop();
+//     } else {
+//       return false      
+//     }
+//   }
+//   return leftSymbols.length === 0;
+// };
+
 var isValid = function(s) {
   if (s.length < 2) {
-    return false
-  }  
-  const leftSymbols = [];
-  for (var i = 0 ; i < s.length; i++) {
+    return false;
+  }
+  if (s.length % 2 !==0) {
+    return false;
+  }
+  const leftSymbol = [];
+  for (var i = 0; i < s.length; i++) {
     if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
-      leftSymbols.push(s[i]);
+      leftSymbol.push(s[i]);
     }
-    else if (s[i] === ')' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '(') {
-      leftSymbols.pop();
+    else if (s[i] === ')' && leftSymbol.length !== 0 && leftSymbol[leftSymbol.length -1] === '(') {
+      leftSymbol.pop();
+    } 
+    else if (s[i] === '}' && leftSymbol.length !== 0 && leftSymbol[leftSymbol.length -1] === '{') {
+      leftSymbol.pop();
     }
-    else if (s[i] === '}' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '{') {
-      leftSymbols.pop();
-    }
-    else if (s[i] === ']' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '[') {
-      leftSymbols.pop();
+    else if (s[i] === ']' && leftSymbol.length !== 0 && leftSymbol[leftSymbol.length -1] === '[') {
+      leftSymbol.pop();
     } else {
-      return false      
+      return false;
     }
   }
-  return leftSymbols.length === 0;
-};
+  return leftSymbol.length === 0;
+}
+
+
