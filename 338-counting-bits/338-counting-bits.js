b602aa51429b61ff4901e7c836190202e47e4fbc
Johnny Wu <skydodle@gmail.com>
2022-09-24 12:17:47 -0700

    Time: 189 ms (22.54%), Space: 49.5 MB (35.90%) - LeetHub

--- 338-counting-bits/338-counting-bits.js
+++ 338-counting-bits/338-counting-bits.js
@@ -3,22 +3,24 @@
  * @return {number[]}
  */
 var countBits = function(n) {
-  // create a output storage array
-  let output = [];
-  // loop the input n from 0
-  for (let i = 0; i <= n; i++) {
-    // declare value set to 0
+  // create an output array
+  // iterate from 0 to n
+    // declare a value set to 0
+    // translate each element into 2 based binaries
+    // iterate through the binaries
+      // add each bit to value
+    // push value to output
+  // return output
+  
+  const output = [];
+  
+  for (let i = 0 ; i <= n; i++) {
     let value = 0;
-    // declare binaryString and assign it to the binary value of current number in outer loop
-    const binaryString = i.toString(2);
-    // loop thru each binary in binaryString
-    for (let j = 0; j < binaryString.length; j++) {
-      // add each binary to value; the addition outcome would be the number of 1s for this i
-      value += Number(binaryString[j])
+    const binary = i.toString(2);
+    for (let j = 0; j < binary.length; j++) {
+      value += Number(binary[j]);
     }
-    // push value to output array, restart outter loop on the next i
     output.push(value);
-  }   
-  // return the output array
+  }
   return output;
 };
