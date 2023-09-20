83c7339b7beca69e3ba68ad599f721748dc59a89
Johnny Wu <skydodle@gmail.com>
2023-09-20 15:41:32 -0700

    Time: 49 ms (85.22%), Space: 43.5 MB (87.02%) - LeetHub

--- 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.js
+++ 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.js
@@ -2,27 +2,46 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var countNegatives = function(grid) {
+// var countNegatives = function(grid) {
 
-    let count = 0;
+//     let count = 0;
 
-    for (let row of grid) {    
-        for (let i = 0; i < row.length; i++) {
-            let last = row.length - 1;
+//     for (let row of grid) {    
+//         for (let i = 0; i < row.length; i++) {
+//             let last = row.length - 1;
 
-            if (row[0] >= 0 && row[last] >= 0) {
-                break;
-            } else if (row[i] < 0 && row[last] < 0) {
-                count += row.length - i;
-                break;
-            }
-        }
-    }
-    return count;
-};
+//             if (row[0] >= 0 && row[last] >= 0) { // if both front and end are positive, discard array
+//                 break;
+//             } else if (row[i] < 0 && row[last] < 0) { // if front positive, move to next until negative
+//                 count += row.length - i;
+//                 break;
+//             }
+//         }
+//     }
+//     return count;
+// }; // Time: O(m * n) Space; O(1)
+
+
 // grid = [
-//     [4,3,2,-1],
-//     [3,2,1,-1],
-//     [1,1,-1,-2],
+//     [ 4, 3, 2,-1],
+//     [ 3, 2, 1,-1],
+//     [ 1, 1,-1,-2],
 //     [-1,-1,-2,-3]
 //     ]
+
+var countNegatives = function(grid) {
+  const height = grid.length;
+  const width = grid[0].length;
+  let r = height - 1;
+  let c = 0;
+  let res = 0;
+  while (r >= 0 && c < width) {
+    if (grid[r][c] >= 0) {
+      c++;
+    } else {
+      res += width - c;
+      r--;
+    }
+  }
+  return res;
+};
