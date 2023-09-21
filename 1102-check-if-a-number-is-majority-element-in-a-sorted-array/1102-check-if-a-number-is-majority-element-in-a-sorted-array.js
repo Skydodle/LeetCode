8d779396e3972b1edd16804028f2dfc4b2601305
Johnny Wu <skydodle@gmail.com>
2023-09-20 19:08:34 -0700

    Time: 50 ms (60.87%), Space: 42.1 MB (60.87%) - LeetHub

--- 1102-check-if-a-number-is-majority-element-in-a-sorted-array/1102-check-if-a-number-is-majority-element-in-a-sorted-array.js
+++ 1102-check-if-a-number-is-majority-element-in-a-sorted-array/1102-check-if-a-number-is-majority-element-in-a-sorted-array.js
@@ -3,18 +3,36 @@
  * @param {number} target
  * @return {boolean}
  */
+// var isMajorityElement = function(nums, target) {
+//     let count = 0; 
+
+//     for (let i = 0; i < nums.length; i++) {
+//       if (nums[i] === target) {
+//         count ++;
+//       }
+//     }
+
+//     if (count > nums.length / 2) {
+//       return true;
+//     }
+
+//     return false;
+// }; // iterative time O(n)
+
+// Binary Search O(log N)
 var isMajorityElement = function(nums, target) {
-    let count = 0; 
-
-    for (let i = 0; i < nums.length; i++) {
-      if (nums[i] === target) {
-        count ++;
-      }
+  let left = 0, right = nums.length - 1, firstIndex = -1;
+  
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+	
+    if (nums[mid] >= target) {
+      if (nums[mid] === target) firstIndex = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
     }
-
-    if (count > nums.length / 2) {
-      return true;
-    }
-
-    return false;
+  }
+  
+  return nums[firstIndex + Math.floor(nums.length / 2)] === target;
 };
