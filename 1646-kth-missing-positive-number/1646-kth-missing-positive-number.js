bda7def317a73d30d7b1bdb8596381d773555bb9
Johnny Wu <skydodle@gmail.com>
2023-09-20 18:32:12 -0700

    Time: 47 ms (88.79%), Space: 42.3 MB (46.68%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.js
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.js
@@ -5,26 +5,21 @@
  */
 var findKthPositive = function(arr, k) {
     let j = 0;
-    let kArray= [];
     let count = 0;
     let max = Number.MAX_SAFE_INTEGER;
 
     for (let i = 1; i <= max ; i++) {
-        // if (kArray.length === k + 1) {
-        //     return kArray[k - 1];
-        // }  
-
-        console.log(j, i, count)
         if (count === k) {
             return i - 1;
         }
+
         if (i == arr[j]) {
             j++;
         } else {
             count++;
         }
     }
-}; // iterative 3 pointer Time O(n + K) Space 
+}; // iterative counter Time O(n + k) Space O(1)
 
 // var findKthPositive = function(arr, k) {
 
