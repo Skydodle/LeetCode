ca8c0dae18c95dfcab067630c30517e64c2bb4e0
Johnny Wu <skydodle@gmail.com>
2023-09-20 18:48:29 -0700

    Time: 54 ms (59.5%), Space: 41.9 MB (80.32%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.js
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.js
@@ -3,24 +3,40 @@
  * @param {number} k
  * @return {number}
  */
-var findKthPositive = function(arr, k) {
-    let j = 0;
-    let count = 0;
-    let max = Number.MAX_SAFE_INTEGER;
-
-    for (let i = 1; i <= max ; i++) {
-        if (count === k) {
-            return i - 1;
-        }
-
-        if (i == arr[j]) {
-            j++;
-        } else {
-            count++;
-        }
-    }
-}; // iterative counter Time O(n + k) Space O(1)
-
 // var findKthPositive = function(arr, k) {
+//     let j = 0;
+//     let count = 0;
+//     let max = Number.MAX_SAFE_INTEGER;
 
-// }
+//     for (let i = 1; i <= max ; i++) {
+//         if (count === k) {
+//             return i - 1;
+//         }
+
+//         if (i == arr[j]) {
+//             j++;
+//         } else {
+//             count++;
+//         }
+//     }
+// }; // iterative counter Time O(n + k) Space O(1)
+
+
+// brute force O(n) time
+var findKthPositive = function(arr, k) {
+    if (k <= arr[0] - 1) {
+        return k;
+    }
+    k -= arr[0] - 1;
+
+    let n  = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let currMissingRange  = arr[i + 1] - arr[i] - 1;
+
+        if (k <= currMissingRange) {
+            return arr[i] + k;
+        }
+        k -= currMissingRange;
+    }
+    return arr[n - 1] + k;
+}
