5cba4d689da794137781d0856adb14f0c3e39e44
Johnny Wu <skydodle@gmail.com>
2023-09-21 17:39:11 -0700

    Time: 40 ms (96.63%), Space: 42 MB (26.21%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.js
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.js
@@ -11,20 +11,32 @@
  * @param {number} n
  * @return {number}
  */
+// var guessNumber = function(n) {
+//     let left = 1;
+//     let right = n;
+
+//     while (left <= right) {
+//         let mid = Math.floor((left + right) / 2);
+//         let clue = guess(mid);
+
+//         if (clue === 0) {
+//             return mid;
+//         } else if (clue === -1) {
+//             right = mid - 1;
+//         } else if (clue === 1) {
+//             left = mid + 1;
+//         }
+//     }
+// };
+
 var guessNumber = function(n) {
-    let left = 1;
-    let right = n;
-
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        let clue = guess(mid);
-
-        if (clue === 0) {
-            return mid;
-        } else if (clue === -1) {
-            right = mid - 1;
-        } else if (clue === 1) {
-            left = mid + 1;
-        }
+    let l = 0, r = n-1;
+    while(l  <= r ){
+        let mid = Math.floor((l+r)/2);
+        let res = guess(mid);
+        if(res == 0) return mid;
+        else if (res == 1) l = mid+1;
+        else r = mid-1;
     }
+    return l;    
 };
