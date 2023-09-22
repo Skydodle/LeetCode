b4b2c2c72e47476e8c03f48301552ab1cfa0497f
Johnny Wu <skydodle@gmail.com>
2023-09-21 17:25:14 -0700

    Time: 75 ms (59.25%), Space: 44.2 MB (31.79%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.js
+++ 0441-arranging-coins/0441-arranging-coins.js
@@ -4,35 +4,35 @@
  */
 
 //Brute Force
-// var arrangeCoins = function(n) {
-//     // 1+2+3 = 6
-//     // 1+2+3+4 = 13
+var arrangeCoins = function(n) {
+    // 1+2+3 = 6
+    // 1+2+3+4 = 13
 
-//     // if 13 - 6 = 7 is < 8
-//     // return 3
+    // if 13 - 6 = 7 is < 8
+    // return 3
 
-//     // 1+2=3
-//     // 1+2+3 = 6
-//     // 6 - 3 = 3 is < 5
-//     // return 2
+    // 1+2=3
+    // 1+2+3 = 6
+    // 6 - 3 = 3 is < 5
+    // return 2
 
-//     if (n < 3) return 1;
+    if (n < 3) return 1;
 
-//     let sum = 0;
-//     for (let i = 1; sum <= n; i++) {
-//         sum += i;
-//         if (sum > n) {
-//             return i - 1;
-//         }
-//     }
-// };
+    let sum = 0;
+    for (let i = 1; sum <= n; i++) {
+        sum += i;
+        if (sum > n) {
+            return i - 1;
+        }
+    }
+};
 
 // improved iterative
-var arrangeCoins = function(n) {
-    let stairs = 0;
+// var arrangeCoins = function(n) {
+//     let stairs = 0;
 
-    while(stairs <= n) {
-        n -= stairs; stairs++; 
-        }
-    return stairs-1
-};
+//     while(stairs <= n) {
+//         n -= stairs; stairs++; 
+//         }
+//     return stairs-1
+// };
