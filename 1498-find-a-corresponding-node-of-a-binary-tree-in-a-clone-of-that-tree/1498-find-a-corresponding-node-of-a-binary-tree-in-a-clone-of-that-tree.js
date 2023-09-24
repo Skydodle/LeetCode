de97c7e5c3a14e39973af6e7e4a75c843f1104b6
Johnny Wu <skydodle@gmail.com>
2023-09-23 19:19:16 -0700

    Time: 208 ms (70.45%), Space: 88.9 MB (5.52%) - LeetHub

--- 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.js
+++ 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.js
@@ -13,29 +13,27 @@
  */
 
 // DFS
-var getTargetCopy = function(original, cloned, target) {
+// var getTargetCopy = function(original, cloned, target) {
 
-    function traverse(oNode, cNode) {
-        if (!oNode) return;
-        if (oNode === target) return cNode;
-        return traverse(oNode.left, cNode.left) || traverse(oNode.right, cNode.right);
-    }
-    return traverse(original, cloned)
-};
+//     function traverse(oNode, cNode) {
+//         if (!oNode) return;
+//         if (oNode === target) return cNode;
+//         return traverse(oNode.left, cNode.left) || traverse(oNode.right, cNode.right);
+//     }
+//     return traverse(original, cloned)
+// };
 
 // BFS
-// var getTargetCopy = function(original, cloned, target) {
+var getTargetCopy = function(original, cloned, target) {
     
-//     let queue = [[original, cloned]];
+    let queue = [[original, cloned]];
     
-//     while(queue.length) {
-//         const next = [];
+    while(queue.length) {
+        const [oNode, cNode] = queue.shift();
+
+        if (oNode === target) return cNode;
         
-//         for(let [oNode, cNode] of queue) {
-//             if(oNode === target) return cNode;
-//             if(oNode.left) next.push([oNode.left, cNode.left]);
-//             if(oNode.right) next.push([oNode.right, cNode.right]);
-//         }
-//         queue = next;
-//     }
-// };
+        if(oNode.left) queue.push([oNode.left, cNode.left]);
+        if(oNode.right) queue.push([oNode.right, cNode.right]);
+    }
+};
