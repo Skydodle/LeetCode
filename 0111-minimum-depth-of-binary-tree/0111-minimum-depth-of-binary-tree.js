cf4f7728319aa0108c57c641059b1ee62bf7ee7d
Johnny Wu <skydodle@gmail.com>
2023-09-24 12:13:54 -0700

    Time: 157 ms (92.99%), Space: 101.1 MB (20.91%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    // if node is null, add no depth
+    if (!root) return 0;
+
+    // if one side doesnt exist, check down other side and +1 for parent
+    if (!root.right) return 1+ + minDepth(root.left);
+    if (!root.left) return 1 + minDepth(root.right);
+
+    // otherwise if both side exist, check down both and find minimum
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+};
