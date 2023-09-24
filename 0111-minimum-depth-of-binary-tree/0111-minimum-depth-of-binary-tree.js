4d5f85156926712d7a490e4669dc4ed6867aff7a
Johnny Wu <skydodle@gmail.com>
2023-09-24 12:46:22 -0700

    Time: 184 ms (39.95%), Space: 99.9 MB (68.84%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -12,14 +12,43 @@
  */
 
 // DFS
+// var minDepth = function(root) {
+//     // if node is null, add no depth
+//     if (!root) return 0;
+
+//     // if one side doesnt exist, check down other side and +1 for parent
+//     if (!root.left) return 1 + minDepth(root.right);
+//     if (!root.right) return 1+ + minDepth(root.left);
+
+//     // otherwise if both side exist, check down both and find minimum
+//     return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+// };
+
 var minDepth = function(root) {
-    // if node is null, add no depth
     if (!root) return 0;
+    let depth = 0;
+    let queue = [root];
 
-    // if one side doesnt exist, check down other side and +1 for parent
-    if (!root.left) return 1 + minDepth(root.right);
-    if (!root.right) return 1+ + minDepth(root.left);
+    while (queue.length) {
+        let size = queue.length;
+        depth++;
 
-    // otherwise if both side exist, check down both and find minimum
-    return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        for (let i = 0; i < size; i++) {
+            let currentNode = queue.shift();
+
+            // Check if it's a leaf node
+            if (!currentNode.left && !currentNode.right) {
+                return depth;
+            }
+
+            if (currentNode.left) {
+                queue.push(currentNode.left);
+            }
+            
+            if (currentNode.right) {
+                queue.push(currentNode.right);
+            }
+        }
+    }
+    return depth; // This line should ideally never be reached for valid inputs.
 };
