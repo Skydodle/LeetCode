e3ff73f48a827398cb62057b5139e2e24060a89d
Johnny Wu <skydodle@gmail.com>
2023-09-24 10:57:28 -0700

    Time: 53 ms (98.26%), Space: 48 MB (56.65%) - LeetHub

---
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var getMinimumDifference = function(root) {
+    let sortedArr = [];
+
+    function inorder(node) {
+        if (!node) return;
+
+        if (node.left) inorder(node.left);
+        sortedArr.push(node.val);
+        if (node.right) inorder(node.right);
+    }
+
+    inorder(root);
+
+    let min = Number.MAX_SAFE_INTEGER;
+    
+    for (let i = 1; i < sortedArr.length; i++){
+        min = Math.min(min, sortedArr[i] - sortedArr[i -1]);
+    }
+
+    return min;
+};
