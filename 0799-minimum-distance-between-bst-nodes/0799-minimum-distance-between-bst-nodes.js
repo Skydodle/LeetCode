927af1fe2787aab0aaa0484238e91a5db556812c
Johnny Wu <skydodle@gmail.com>
2023-09-24 09:29:12 -0700

    Time: 65 ms (6.54%), Space: 44.2 MB (5.77%) - LeetHub

---
+++ 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var minDiffInBST = function(root) {
+    let inorderValues = [];
+    
+    // Helper function to perform the inorder traversal left root right
+    function inorder(node) {
+        if (!node) return;
+        inorder(node.left);
+        inorderValues.push(node.val);
+        inorder(node.right);
+    }
+    
+    inorder(root);  // get the sorted list of BST values
+    console.log(inorderValues)
+
+    let min = Number.MAX_SAFE_INTEGER;
+    for (let i = 1; i < inorderValues.length; i++) {
+        min = Math.min(min, inorderValues[i] - inorderValues[i-1]);
+    }
+    
+    return min;
+};
