07969f8847ba438e442800d5eab0836b41adcc50
Johnny Wu <skydodle@gmail.com>
2023-09-26 14:17:26 -0700

    Time: 55 ms (67.03%), Space: 44 MB (79.39%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.js
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.js
@@ -10,25 +10,47 @@
  * @param {TreeNode} root
  * @return {number}
  */
+
+// BFS 
+// var sumOfLeftLeaves = function(root) {
+//     if (!root.left && !root.right) return 0;
+//     let sum = 0;
+//     let queue = [root];
+
+//     while (queue.length){
+//         let node = queue.shift();
+
+//         if (node.left) { // if left child exist
+//             if (!node.left.left && !node.left.right) { // and its a leaf
+//                 sum += node.left.val; // add leaf node val to sum
+//             } else {
+//                 queue.push(node.left);
+//             }
+//         }
+
+//         if (node.right) {
+//             queue.push(node.right); // so we can check right child's left children
+//         }
+//     }
+//     return sum;
+// };
+// Time: O(N) Space: O(N)
+
+// DFS
 var sumOfLeftLeaves = function(root) {
-    if (!root.left && !root.right) return 0;
-    let sum = 0;
-    let queue = [root];
-
-    while (queue.length){
-        let node = queue.shift();
-
-        if (node.left) { // if left child exist
-            if (!node.left.left && !node.left.right) { // and its a leaf
-                sum += node.left.val; // add leaf node val to sum
-            } else {
-                queue.push(node.left);
-            }
+    
+    function isLeaveNode(root){
+        if(root && root.left == null && root.right == null) {
+            return true
         }
+        return false
+    }    
+    
+    if(!root) return 0
 
-        if (node.right) {
-            queue.push(node.right); // so we can check right child's left children
-        }
+    if(isLeaveNode(root.left)){
+        return root.left.val + sumOfLeftLeaves(root.right)
     }
-    return sum;
+    
+    return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right)
 };
