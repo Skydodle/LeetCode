8cae5669d154647349c3672055bc099620751c90
Johnny Wu <skydodle@gmail.com>
2023-09-27 13:07:04 -0700

    Time: 56 ms (45.62%), Space: 42 MB (83.29%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+  // base case: strs have no common prefix
+  if (str1 + str2 !== str2 + str1) return '';
+
+  // Euclidean algorithm to find GCD between two lengths
+  const gcds = (a, b) => {
+    if (!b) return a;
+
+    return gcds(b, a % b)
+  }
+
+  let length = gcds(str1.length, str2.length);
+
+  return str1.slice(0, length)
+};
