f68864f08f8f5f699beee6584cf5543b935275f8
Johnny Wu <skydodle@gmail.com>
2023-09-27 17:55:25 -0700

    Time: 47 ms (79.68%), Space: 41.3 MB (93.42%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.js
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.js
@@ -2,20 +2,18 @@
  * @param {number[]} gain
  * @return {number}
  */
-var largestAltitude = function(gain) {
-    let altitude = [0];
-    let highest = -Infinity;
+// var largestAltitude = function(gain) {
+//     let altitude = [0];
+//     let highest = -Infinity;
 
-    for (let i = 0; i < gain.length; i++){
-        altitude.push(gain[i] + altitude[i]);
-    }
-
-    for (let j = 0 ; j < altitude.length; j++){
-        highest = Math.max(highest, altitude[j]);
-    }
-
-    return highest;
-};
+//     for (let i = 0; i < gain.length; i++){
+//         altitude.push(gain[i] + altitude[i]);
+//     }
+//     for (let j = 0 ; j < altitude.length; j++){
+//         highest = Math.max(highest, altitude[j]);
+//     }
+//     return highest;
+// };
 
 
  
@@ -23,3 +21,16 @@ var largestAltitude = function(gain) {
 // gain  [-5, 1, 5, 0,-7]
 // alt [0,-5,-4, 1, 1,-6]
 //.     0  1. 2  3  4  5
+
+// Improved
+var largestAltitude = function(gain) {
+    let highest = 0; // starting point
+    let currentAltitude = 0;
+
+    for (let point = 0; point < gain.length; point++) {
+        currentAltitude += gain[point];
+        highest = Math.max(highest, currentAltitude);
+    }
+
+    return highest;
+}
