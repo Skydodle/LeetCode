4bbba8a317a7ba905a827d4d8380aff821f170cf
Johnny Wu <skydodle@gmail.com>
2023-09-28 21:32:54 -0700

    Time: 60 ms (40.66%), Space: 43.9 MB (86.86%) - LeetHub

--- 0904-leaf-similar-trees/0904-leaf-similar-trees.js
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.js
@@ -17,16 +17,17 @@ var leafSimilar = function(root1, root2) {
 
   function dfs(node, whichRoot) {
     if (!node) return;
-    if (!node.left && !node.right){
+    if (!node.left && !node.right){ // found the leaf node
       whichRoot === 'root1' ? leafs1.push(node.val) : leafs2.push(node.val);
       return;
     }
+    // not leaf node, keep going down
     dfs(node.left, whichRoot);
     dfs(node.right, whichRoot);
   }
 
-  dfs(root1, 'root1');
-  dfs(root2, 'root2');
+  dfs(root1, 'root1'); // push all root1 leaf nodes
+  dfs(root2, 'root2'); // push all root2 leaf nodes
 
-  return leafs1.toString() === leafs2.toString();
+  return leafs1.toString() === leafs2.toString(); // cant compare arrays directly
 };
