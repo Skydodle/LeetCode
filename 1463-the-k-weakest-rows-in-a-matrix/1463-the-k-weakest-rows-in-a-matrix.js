ed5dac15ae82d3feb741bd49a0fcbd0fc67da63d
Johnny Wu <skydodle@gmail.com>
2023-10-02 16:23:38 -0700

    Time: 57 ms (48.87%), Space: 44.1 MB (63.53%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.js
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.js
@@ -4,21 +4,24 @@
  * @return {number[]}
  */
 var kWeakestRows = function(mat, k) {
-    let soldiers = [];
+    let soldiers = []; // holds the count of soldiers for each row
 
+    // Iterate thru each row and track the soldier count
     for (let i = 0; i < mat.length; i++) {
         let row = mat[i];
         let count = 0;
-        // console.log(row)
 
         for (let j = 0; j < row.length; j++) {
             if (row[j] === 1) {
                 count++;
+            } else {
+                break;
             }
         }
         soldiers.push(count);
     }
 
+    // Create the idx array
     let sortIdx = [];
 
     for (let i = 0; i < soldiers.length; i++) {
@@ -26,15 +29,9 @@ var kWeakestRows = function(mat, k) {
     }
 
     sortIdx.sort((a, b) => {
-        if (soldiers[a] === soldiers[b]) return a - b;
-        return soldiers[a] - soldiers[b]
+        if (soldiers[a] === soldiers[b]) return a - b; // if same value, sort by idx
+        return soldiers[a] - soldiers[b] // else sort by value
     })
 
     return sortIdx.slice(0, k)
 };
-
-//  [1,1,0,0,0],
-//  [1,1,1,1,0],
-//  [1,0,0,0,0],
-//  [1,1,0,0,0],
-//  [1,1,1,1,1]]
