0d0a62e7be833fd7631b43e55af958d4d1309a1c
Johnny Wu <skydodle@gmail.com>
2023-10-01 19:55:53 -0700

    Time: 81 ms (11.36%), Space: 41.4 MB (86.14%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.js
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -7,3 +7,19 @@ var fib = function(n) {
     
     return fib(n - 1) + fib(n - 2)
 };
+
+
+// var fib = function (n) {
+//   let a = 0;
+//   let b = 1;
+//   let count = 2;
+//   let c;
+//   if (n == 0 || n == 1) return n;
+//   while (count <= n) {
+//     c = a + b;
+//     a = b;
+//     b = c;
+//     count++;
+//   }
+//   return c;
+// };
