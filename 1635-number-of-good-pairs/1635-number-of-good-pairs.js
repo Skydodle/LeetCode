e5ae0f8d5274beb4723438811d928ddb9bacd779
Johnny Wu <skydodle@gmail.com>
2023-10-02 17:48:06 -0700

    Time: 52 ms (54.44%), Space: 41.8 MB (52.31%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.js
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.js
@@ -2,16 +2,36 @@
  * @param {number[]} nums
  * @return {number}
  */
+// Brute force: time O(N^2) space O(1)
+// var numIdenticalPairs = function(nums) {
+//     let count = 0;
+
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] === nums[j]) {
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// };
+
 var numIdenticalPairs = function(nums) {
     let count = 0;
+    let map = {};
 
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] === nums[j]) {
-                count++;
-            }
-        }
+    // Track count of each num
+    for (let num of nums) {
+        map[num] = (map[num] || 0) + 1;
     }
 
+    // Calculate # of good pairs
+    for (let key in map) {
+        let n = map[key]; // get the value
+        count += (n * (n -1)) /2; // formula to find good pairs
+    }
     return count;
-};
+}
+
+// Time: O(N)
+// Space: O(N)
