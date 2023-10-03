00617584e677605af585602871af0f8e1e78438d
Johnny Wu <skydodle@gmail.com>
2023-10-02 18:47:40 -0700

    Time: 61 ms (17.67%), Space: 43.8 MB (45.03%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.js
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.js
@@ -9,14 +9,14 @@ var diagonalSum = function(mat) {
 
     for (let row = 0; row < mat.length; row++) {
         sum += mat[row][col];
-        seen.add([row,col].toString());
+
         col++;
     }
 
     col = 0;
 
     for (let row = mat.length - 1; row >= 0; row--) {
-        if (!seen.has(`${row},${col}`)) {
+        if (row !== col) {
             sum += mat[row][col];
         }
         col++;
