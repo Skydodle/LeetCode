085fc0704a745586dae015c033eb397d1fe31bde
Johnny Wu <skydodle@gmail.com>
2023-10-02 17:04:35 -0700

    Time: 63 ms (54.31%), Space: 44.4 MB (59.67%) - LeetHub

---
+++ 0898-transpose-matrix/0898-transpose-matrix.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+var transpose = function(matrix) {
+    let flipped = Array(matrix[0].length).fill(null).map(() => []);
+    
+    for (let i = 0; i < matrix.length; i++) {
+        let row =  matrix[i];
+
+        for (let j = 0; j < row.length; j++) {
+            flipped[j].push(row[j])
+        }
+    }
+   return flipped;
+};
+
+   
+    //    [1,2,3],
+    //    [4,5,6],
+    //    [7,8,9]
+       
+    //    [1,4,7],
+    //    [2,5,8],
+    //    [3,6,9]
+
+    // [] [] []
+    //. 0. 1  2
+       
