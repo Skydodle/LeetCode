99386d2fa1b5f9a438fc98248f1fd2e5b65d1c7f
Johnny Wu <skydodle@gmail.com>
2023-10-03 22:07:13 -0700

    Time: 39 ms (100%), Space: 42.4 MB (82%) - LeetHub

--- 0422-valid-word-square/0422-valid-word-square.js
+++ 0422-valid-word-square/0422-valid-word-square.js
@@ -2,19 +2,33 @@
  * @param {string[]} words
  * @return {boolean}
  */
-var validWordSquare = function(words) {
+// var validWordSquare = function(words) {
 
-    for (let row = 0; row < words.length; row++) {
-        let column = '';
+//     for (let row = 0; row < words.length; row++) {
+//         let column = '';
         
+//         for (let col = 0; col < words[row].length; col++) {
+//             if (!words[col]) return false;
+//             column += words[col][row];
+//         }
+//         if (column !== words[row]) return false;
+//     }
+//     return true;
+// };
+// Time: O(M)
+// Space: O(1)
+
+// Improved without building column string
+var validWordSquare = function(words) {
+    for (let row = 0; row < words.length; row++) {
         for (let col = 0; col < words[row].length; col++) {
-            if (!words[col]) return false;
-            column += words[col][row];
+            // Check existence of words[col] and words[col][row] 
+            if (!words[col] || !words[col][row]) return false;
+            // Check if the characters match for both row and column
+            if (words[col][row] !== words[row][col]) return false;
         }
-        if (column !== words[row]) return false;
     }
     return true;
 };
 // Time: O(M)
 // Space: O(1)
-
