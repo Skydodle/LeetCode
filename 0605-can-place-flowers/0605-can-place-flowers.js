768622d2effcdd26394f3c3522e5a49ef63f5ede
Johnny Wu <skydodle@gmail.com>
2023-10-09 13:38:06 -0700

    Time: 62 ms (34.5%), Space: 44.6 MB (30.29%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -3,22 +3,41 @@
  * @param {number} n
  * @return {boolean}
  */
+// var canPlaceFlowers = function(flowerbed, n) {
+
+//     let count = 0;
+//     if (flowerbed.length === 1 && flowerbed[0] === 0) count = 1;
+
+//     for (let i = 0; i < flowerbed.length; i++) {
+//       if (flowerbed[i] === 0) {
+//         if (
+//           (i === 0 && flowerbed[i + 1] === 0) || 
+//           (flowerbed[i+1] === 0 && flowerbed[i-1] === 0)
+//           ) {
+//           count++;
+//           flowerbed[i] = 1;
+//         } 
+//         if (i === flowerbed.length -1 && flowerbed[i - 1] === 0) count++;
+//       }
+//     }
+//     return count >= n;
+// };
+
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
 var canPlaceFlowers = function(flowerbed, n) {
-
-    let count = 0;
-    if (flowerbed.length === 1 && flowerbed[0] === 0) count = 1;
-
-    for (let i = 0; i < flowerbed.length; i++) {
-      if (flowerbed[i] === 0) {
-        if (
-          (i === 0 && flowerbed[i + 1] === 0) || 
-          (flowerbed[i+1] === 0 && flowerbed[i-1] === 0)
-          ) {
-          count++;
-          flowerbed[i] = 1;
-        } 
-        if (i === flowerbed.length -1 && flowerbed[i - 1] === 0) count++;
-      }
+  for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+    if (
+      flowerbed[i] === 0 &&
+      flowerbed[i - 1] !== 1 &&
+      flowerbed[i + 1] !== 1
+    ) {
+      n--;
+      i++;
     }
-    return count >= n;
+  }
+  return n === 0;
 };
