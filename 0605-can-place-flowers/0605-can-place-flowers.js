3a5f0486555e8b95a3f00e8033aa0fa6297c8793
Johnny Wu <skydodle@gmail.com>
2023-10-09 13:30:36 -0700

    Time: 72 ms (9.22%), Space: 44.6 MB (30.29%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+
+    let count = 0;
+    if (flowerbed.length === 1 && flowerbed[0] === 0) count = 1;
+
+    for (let i = 0; i < flowerbed.length; i++) {
+      if (flowerbed[i] === 0) {
+        if (i === 0 && flowerbed[i + 1] === 0) {
+          count++;
+          flowerbed[i] = 1;
+        } 
+        if (i === flowerbed.length -1 && flowerbed[i - 1] === 0) count++;
+        if (flowerbed[i+1] === 0 && flowerbed[i-1] === 0){
+          count++;
+          flowerbed[i] = 1;
+        }
+      }
+    }
+    console.log(count)
+    return count >= n;
+};
