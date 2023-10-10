8ff84eec0b6d5c9ef69541280f441b4e52e184c0
Johnny Wu <skydodle@gmail.com>
2023-10-09 21:13:27 -0700

    Time: 385 ms (15.84%), Space: 68 MB (5.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,20 +3,44 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    
-    const answer = Array(nums.length).fill(0);
+    const n = nums.length;
+    const leftProducts = new Array(n).fill(1);
+    const rightProducts = new Array(n).fill(1);
+    const answer = new Array(n);
 
-    for (let i = 0; i < nums.length; i++) {
-        let base = 1;
-        for (let j = 0; j < nums.length; j++) {
-            if (j === i) {
-                continue;
-            } else {
-                base *= nums[j];
-            }
-        }
-        answer[i] = base;
+    // Compute leftProducts
+    let leftBase = 1;
+    for (let i = 0; i < n; i++) {
+        leftProducts[i] = leftBase;
+        leftBase *= nums[i];
+    }
+
+    // console.log(leftProducts)
+
+    // Compute rightProducts
+    let rightBase = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        rightProducts[i] = rightBase;
+        rightBase *= nums[i];
+        console.log(rightBase)
+    }
+
+    // console.log(rightProducts)
+
+    // Compute answer
+    for (let i = 0; i < n; i++) {
+        answer[i] = leftProducts[i] * rightProducts[i];
     }
 
     return answer;
 };
+
+//                ------->
+//        nums = [1,2,3,4]
+//        left    1 1 2 6
+//        base    1 2 6 24
+
+//                <--------       
+//        nums = [4, 3, 2, 1]
+//        right   1  4 12 24
+//        base    4 12 24 24
