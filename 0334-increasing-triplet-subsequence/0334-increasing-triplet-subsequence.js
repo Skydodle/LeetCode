9402b40f9ed63a57e3e59555cb7751c2e1d30a36
Johnny Wu <skydodle@gmail.com>
2023-10-12 09:35:15 -0700

    Time: 51 ms (99.83%), Space: 60.1 MB (94.9%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
@@ -20,4 +20,10 @@ var increasingTriplet = function(nums) {
     return false;
 };
 
-// Use greedy
+// Use greedy because no backtracking and we are making optimal choice at every step in order
+// to find global optimum
+// we are updating the smallest and 2nd smallest at every step
+// as soon a smaller is found, the first resets to that num
+
+// Time: O(N)
+// Space: O(1)
