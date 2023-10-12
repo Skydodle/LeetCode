32407b614faa0afd4096f1283122fc01b4b8c48a
Johnny Wu <skydodle@gmail.com>
2023-10-12 10:56:36 -0700

    Time: 53 ms (89.17%), Space: 44.4 MB (54.92%) - LeetHub

--- 0443-string-compression/0443-string-compression.js
+++ 0443-string-compression/0443-string-compression.js
@@ -3,23 +3,31 @@
  * @return {number}
  */
 var compress = function(chars) {
-    let k = 0; // to overwrite the original array
-    let count = 1; // count of consecutive characters
+    let k = 0;  // Pointer for writing into the array
+    let count = 1;  // Count of the character being counted
 
     for (let i = 0; i < chars.length; i++) {
-        if (chars[i] === chars[i + 1]) {
-            count++;
-        } else {
+        // Check for the end of a group of repeating characters or the end of the array
+        if (i === chars.length - 1 || chars[i] !== chars[i + 1]) {
+            // Write the character and possibly the count into the array
             chars[k++] = chars[i];
             if (count > 1) {
-                let countStr = count.toString();
+                const countStr = count.toString();
                 for (let j = 0; j < countStr.length; j++) {
                     chars[k++] = countStr[j];
                 }
             }
-            count = 1; // reset count
+            
+            // Reset the count
+            count = 1;
+        } else {
+            // Increment the count for the current group of repeating characters
+            count++;
         }
     }
+
+    // Trim the array to its new length
+    chars.length = k;
     return k;
 };
 
