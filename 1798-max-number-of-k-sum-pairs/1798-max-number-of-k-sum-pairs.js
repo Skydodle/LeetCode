e0f7e4e4f8ec4caa0af59f671a66dbdd24d82e39
Johnny Wu <skydodle@gmail.com>
2023-10-12 17:40:08 -0700

    Time: 133 ms (68%), Space: 52.8 MB (83.42%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.js
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.js
@@ -3,59 +3,55 @@
  * @param {number} k
  * @return {number}
  */
-// var maxOperations = function(nums, k) {
-//     nums = nums.sort((a, b) => a - b); // O (NlogN)
-//     let count = 0;
+var maxOperations = function(nums, k) {
+    nums = nums.sort((a, b) => a - b); // O (NlogN)
+    let count = 0;
 
-//     for (let left = 0 , right = nums.length - 1; left < right;) {
-//         let difference = k - nums[right];
-//         if (nums[left] === difference) {
-//             left++;
-//             right--;
-//             count++;
-//         } else if (nums[left] < difference) {
-//             left++;
-//         } else  {
-//             right--;
-//         }
-//     }
-//     return count;
-// };
+    for (let left = 0 , right = nums.length - 1; left < right;) {
+        let difference = k - nums[right];
+        if (nums[left] === difference) {
+            left++;
+            right--;
+            count++;
+        } else if (nums[left] < difference) {
+            left++;
+        } else  {
+            right--;
+        }
+    }
+    return count;
+};
 
 // [
 //   1, 1, 2, 3, 3, 3, 4
 // ]
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
-var maxOperations = function(nums, k) {
-    const map = new Map();
-    let count = 0;
 
-    // Populate the HashMap with the occurrences of each number.
-    for (let num of nums) {
-        map.set(num, (map.get(num) || 0) + 1);
-    }
+// var maxOperations = function(nums, k) {
+//     const map = new Map();
+//     let count = 0;
 
-    // Iterate through the array to find pairs that sum up to k.
-    for (let num of nums) {
-        let complement = k - num;
+//     // Populate the HashMap with the occurrences of each number.
+//     for (let num of nums) {
+//         map.set(num, (map.get(num) || 0) + 1);
+//     }
 
-        if (map.get(num) > 0 && map.get(complement) > 0) {
-            // Check if the number and its complement are the same.
-            // If they are, there must be at least two instances to form a pair.
-            if (num === complement && map.get(num) < 2) {
-                continue;
-            }
+//     // Iterate through the array to find pairs that sum up to k.
+//     for (let num of nums) {
+//         let complement = k - num;
 
-            // Perform the operation.
-            count++;
-            map.set(num, map.get(num) - 1);
-            map.set(complement, map.get(complement) - 1);
-        }
-    }
+//         if (map.get(num) > 0 && map.get(complement) > 0) {
+//             // Check if the number and its complement are the same.
+//             // If they are, there must be at least two instances to form a pair.
+//             if (num === complement && map.get(num) < 2) {
+//                 continue;
+//             }
 
-    return count;
-};
+//             // Perform the operation.
+//             count++;
+//             map.set(num, map.get(num) - 1);
+//             map.set(complement, map.get(complement) - 1);
+//         }
+//     }
+
+//     return count;
+// };
