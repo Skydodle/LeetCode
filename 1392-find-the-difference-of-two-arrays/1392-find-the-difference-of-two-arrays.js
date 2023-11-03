93824b928b23b6b923e761a885eac8fb1b25af10
Johnny Wu <skydodle@gmail.com>
2023-11-03 11:41:53 -0700

    Time: 80 ms (69.59%), Space: 49 MB (31.74%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
@@ -25,3 +25,13 @@ var findDifference = function(nums1, nums2) {
 
     return answer;
 };
+
+// Input: nums1 = [1,2,3], nums2 = [2,4,6]
+// Output: [[1,3],[4,6]]
+
+// let ans1=new Set(nums1)
+// nums2.forEach(v=>{ans1.delete(v)});
+// let ans2=new Set(nums2);
+// nums1.forEach(v=>{ans2.delete(v)});    
+
+// return [[...ans1],[...ans2]];
