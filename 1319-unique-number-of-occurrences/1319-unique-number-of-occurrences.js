a40a0e3906e178127955715c4286d42b7b4c4e10
Johnny Wu <skydodle@gmail.com>
2023-11-03 18:32:49 -0700

    Time: 56 ms (39.65%), Space: 42.6 MB (57.5%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.js
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.js
@@ -3,16 +3,14 @@
  * @return {boolean}
  */
 var uniqueOccurrences = function(arr) {
-    const map = {};
+    const map = {}
 
     for (let i = 0; i < arr.length; i++) {
-        map[arr[i]] = map[arr[i]] === undefined ? 1 : map[arr[i]] + 1; 
+        map[arr[i]] = map[arr[i]] === undefined ? 1 : map[arr[i]] + 1;
     }
 
     const count = Object.values(map);
-    const unique = new Set(count);
+    const unqiue = new Set(count);
 
-   return count.length === unique.size;
+    return count.length === unqiue.size;
 };
-
-// Time: O(N). Space: O(N)
